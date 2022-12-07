0b21f30939834e9bd450c39cacdd6c682afd41e1
Andrei Popa <35630083+andreipopa90@users.noreply.github.com>
2022-12-07 11:57:09 +0100

    Day 7

--- Solutions/Day6.cpp
+++ Solutions/Day6.cpp
@@ -3,7 +3,7 @@
 //
 
 #include <iostream>
-#include <map>
+#include <set>
 #include <vector>
 #include <algorithm>
 #include "Day6.h"
@@ -16,25 +16,24 @@ void Day6::ReadFile() {
     std::cout << "Day 6" << std::endl;
     std::ifstream fin("../Input/Day6.txt");
     std::getline(fin, dataStream);
-
+    fin.close();
 }
 
 void Day6::Solution1() {
 
-    bool found = false;
-    std::vector<char> visited;
+    bool found;
+    std::set<char> visited;
     int foundIndex = 0;
 
-    for (int i = 0; i < dataStream.size() - 4 && !found; i++) {
+    for (int i = 0; i < dataStream.size() - 4; i++) {
         std::string subString = dataStream.substr(i, 4);
-        visited.clear();
         found = true;
-        for (int j = 0; j < subString.size(); j++) {
-            if (std::count(visited.begin(), visited.end(), subString[j])) {
+        for (char & j : subString) {
+            if (!visited.insert(j).second) {
                 found = false;
+                visited.clear();
                 break;
             }
-            visited.push_back(subString[j]);
         }
         if (found) {
             foundIndex = i + 4;
@@ -47,20 +46,19 @@ void Day6::Solution1() {
 
 void Day6::Solution2() {
 
-    bool found = false;
-    std::vector<char> visited;
+    bool found;
+    std::set<char> visited;
     int foundIndex = 0;
 
-    for (int i = 0; i < dataStream.size() - 14 && !found; i++) {
+    for (int i = 0; i < dataStream.size() - 14; i++) {
         std::string subString = dataStream.substr(i, 14);
-        visited.clear();
         found = true;
-        for (int j = 0; j < subString.size(); j++) {
-            if (std::count(visited.begin(), visited.end(), subString[j])) {
+        for (char & j : subString) {
+            if (!visited.insert(j).second) {
                 found = false;
+                visited.clear();
                 break;
             }
-            visited.push_back(subString[j]);
         }
         if (found) {
             foundIndex = i + 14;
--- Solutions/Day7.cpp
+++ Solutions/Day7.cpp
@@ -3,22 +3,114 @@
 //
 
 #include <iostream>
+#include <map>
 #include "Day7.h"
+#include "fstream"
+#include "sstream"
+#include "vector"
+
+struct Folder{
+    int size;
+    std::vector<Folder*> next;
+    Folder *prev;
+    std::string name;
+}*root;
+
+Folder* FindFolder(Folder* folder, const std::string& folderName) {
+    if (folder->name == folderName && folderName == "/") return root;
+    for(auto& i : folder->next) {
+        if (i->name == folderName) return i;
+    }
+    return nullptr;
+}
+
+void FindSizes(Folder* folder, int &size) {
+    for (auto& i: folder->next) {
+        size = i->size <= 100000 ? size + i->size : size;
+        FindSizes(i, size);
+    }
+}
+
+void FindHighestSizes(Folder* folder, int minimumToDelete, int& sizeToDelete) {
+
+    for (auto& i: folder->next) {
+        if (i->size >= minimumToDelete && i->size < sizeToDelete)
+            sizeToDelete = i->size;
+        FindHighestSizes(i, minimumToDelete, sizeToDelete);
+    }
+
+}
 
 void Day7::ReadFile() {
 
     std::cout << "Day 7" << std::endl;
+    std::ifstream fin("../Input/Day7.txt");
+    std::string inputLine, inputToken;
 
+    root = new Folder;
+    root->size = 0;
+    root->name = "/";
+    root->prev = nullptr;
+    root->next = static_cast<std::vector<Folder *>>(0);
+    Folder* currentFolder = root;
+
+    while (std::getline(fin, inputLine)) {
+        std::stringstream splitter(inputLine);
+
+        // Don't care about ls
+        if (inputLine[0] == '$' && inputLine.find("cd") != -1) {
+            while(std::getline(splitter, inputToken, ' ')) {}
+            // Go down or up one level
+            if (inputToken != "..")
+                currentFolder = FindFolder(currentFolder, inputToken);
+            else {
+                currentFolder = currentFolder->prev;
+            }
+        }
+
+        // Add new directory to the folder structure
+        else if (inputLine.find("dir") != -1) {
+            while(std::getline(splitter, inputToken, ' ')) {}
+            auto* newFolder = new Folder;
+            newFolder->name = inputToken;
+            newFolder->prev = currentFolder;
+            newFolder->next = static_cast<std::vector<Folder *>>(0);
+            newFolder->size = 0;
+            currentFolder->next.push_back(newFolder);
+        }
+
+        // Update size of directory and all sizes until the root
+        else if ('0' <= inputLine[0] && inputLine[0] <= '9') {
+            std::getline(splitter, inputToken, ' ');
+            currentFolder->size += stoi(inputToken);
+            Folder* iterator = currentFolder;
+            while (iterator->name != "/") {
+                iterator = iterator->prev;
+                iterator->size += stoll(inputToken);
+            }
+        }
+        // Clear the string stream
+        splitter.clear();
+        splitter.str(std::string());
+    }
+
+    fin.close();
 }
 
 void Day7::Solution1() {
 
-    std::cout << "Solution 1 of Day 7 is: " << std::endl;
+    int size = 0;
+    FindSizes(root, size);
+    std::cout << "Solution 1 of Day 7 is: " << size << std::endl;
 
 }
 
 void Day7::Solution2() {
-
-    std::cout << "Solution 2 of Day 7 is: " << std::endl;
-
+    int maximumSize = 70000000;
+    int minimumNeeded = 30000000;
+    int minimumToDelete = minimumNeeded - maximumSize + root->size;
+    // Start from root cause that is the biggest one at first and would free the most space
+    int directorySizeToDelete = root->size;
+    FindHighestSizes(root, minimumToDelete, directorySizeToDelete);
+    std::cout << "Solution 2 of Day 7 is: " << directorySizeToDelete << std::endl;
 }
