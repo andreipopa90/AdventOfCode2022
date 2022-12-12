69d445e22d09365d7b3383553b709e10823979ed
Andrei Popa <35630083+andreipopa90@users.noreply.github.com>
2022-12-12 10:45:17 +0100

    Day 12

--- Solutions/Day12.cpp
+++ Solutions/Day12.cpp
@@ -20,7 +20,6 @@ std::pair<int, int> start, end, dir[4]{
         {1, 0},
         {0, 1}
 };
-std::queue<std::pair<int, int>> startPoints;
 
 void Day12::ReadFile() {
 
@@ -32,11 +31,7 @@ void Day12::ReadFile() {
             fin >> heightMap[i][j];
             if (heightMap[i][j] == 'S') {
                 start = {i, j};
-                startPoints.push(start);
                 heightMap[i][j] = 'a';
-            }
-            else if (heightMap[i][j] == 'a') {
-                startPoints.emplace(i, j);
             }
             if (heightMap[i][j] == 'E') {
                 end = {i, j};
@@ -49,75 +44,53 @@ void Day12::ReadFile() {
 
 }
 
-int RoadSizes(std::pair<int, int> startPoint, std::pair<int, int> endPoint) {
-    std::vector<std::pair<int, int>> visited;
+void bfs(int direction, std::map<std::pair<int, int>, int> &roadSize, std::pair<int, int> startPoint){
     std::queue<std::pair<int, int>> queue;
-    std::map<std::pair<int, int>, std::vector<std::pair<int, int>>> parent;
-    visited.push_back(startPoint);
     queue.push(startPoint);
     std::pair<int, int> currentPosition;
-    std::pair<int, int> endParent = {-1, -1};
+
+    roadSize[startPoint] = 0;
 
     while (!queue.empty()) {
         currentPosition = queue.front();
         queue.pop();
-        if (currentPosition != endPoint) {
-            for (auto & i : dir) {
-                std::pair<int, int> nextDirection = {currentPosition.first + i.first, currentPosition.second + i.second};
-                if (nextDirection.first >= 0 && nextDirection.first < ROW &&
-                    nextDirection.second >= 0 && nextDirection.second < COL)
-                {
-                    int difference = heightMap[currentPosition.first][currentPosition.second] -
-                                     heightMap[nextDirection.first][nextDirection.second];
-                    if (difference >= - 1 &&
-                        std::find(visited.begin(), visited.end(),nextDirection) == visited.end()) {
-                        visited.push_back(nextDirection);
-                        queue.push(nextDirection);
-                        parent[currentPosition].push_back(nextDirection);
-                        if (nextDirection == endPoint) {
-                            endParent = currentPosition;
-                        }
-                    }
+        for (auto & i : dir) {
+            std::pair<int, int> nextDirection = {currentPosition.first + i.first, currentPosition.second + i.second};
+            if (nextDirection.first >= 0 && nextDirection.first < ROW &&
+                nextDirection.second >= 0 && nextDirection.second < COL)
+            {
+                int difference = heightMap[currentPosition.first][currentPosition.second] -
+                                 heightMap[nextDirection.first][nextDirection.second];
+                if (direction * difference >= -1 && roadSize.find(nextDirection) == roadSize.end()) {
+                    queue.push(nextDirection);
+                    roadSize[nextDirection] = roadSize[currentPosition] + 1;
                 }
             }
         }
-        else{
-            break;
-        }
     }
-
-    int roadSize = 1;
-
-    if (endParent.first == -1 && endParent.second == -1) return -1;
-
-    while(endParent != start) {
-        for (const auto& x: parent) {
-            if (std::find(x.second.begin(), x.second.end(), endParent) != x.second.end()) {
-                endParent = x.first;
-                roadSize += 1;
-                break;
-            }
-        }
-    }
-
-    return roadSize;
 }
 
 void Day12::Solution1() {
-    std::cout << "Solution 1 of Day 12 is: " << RoadSizes(start, end) << std::endl;
+    std::map<std::pair<int, int>, int> roadSize;
+
+    bfs(1, roadSize, start);
+
+    std::cout << "Solution 1 of Day 12 is: " << roadSize[end] << std::endl;
 }
 
 void Day12::Solution2() {
+    std::map<std::pair<int, int>, int> roadSize;
 
-    std::cout << startPoints.size() << std::endl;
-    std::vector<int> roadSizes;
+    bfs(-1, roadSize, end);
 
-    while(!startPoints.empty()) {
-        roadSizes.push_back(RoadSizes(startPoints.front(), end));
-        startPoints.pop();
+    int minimumRoad = INT32_MAX;
+
+    for (auto const& road: roadSize) {
+        if (heightMap[road.first.first][road.first.second] == 'a' && road.second < minimumRoad) {
+            minimumRoad = road.second;
+        }
     }
 
-    int maximum = *std::max_element(std::begin(roadSizes), std::end(roadSizes));
-    std::cout << "Solution 2 of Day 12 is: " << maximum << std::endl;
+    std::cout << "Solution 2 of Day 12 is: " << minimumRoad << std::endl;
 
 }
